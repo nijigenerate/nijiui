9671706be3197f07d914a900380ae1214866bf1e
Lin, Yong Xiang <r888800009@gmail.com>
2025-09-09 03:35:21 +0800

    chore: sync `uiOpenLink` implementation with nijigenerate's `incOpenLink`

--- source/nijiui/utils/link.d
+++ source/nijiui/utils/link.d
@@ -12,11 +12,5 @@ import std.process;
     Opens a link with the user's preferred webbrowser
 */
 void uiOpenLink(string link) {
-    version(Windows) {
-        spawnShell("start " ~ link);
-    } else version(OSX) {
-        spawnShell("open " ~ link);
-    } else version(Posix) {
-        spawnShell("xdg-open " ~ link);
-    }
+    browse(link);
 }
